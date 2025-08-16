3d2ddfcc00c7e23b6eadef874aa235c76999afaf
gladyskankwala <gladyskankwala03@gmail.com>
2025-08-16 22:33:55 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Hero from "./components/Hero"
+
+function App() {
+  return (
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Hero></Hero>
+    </main>
+  )
+}
+
+export default App
