0193fdb3490b01afed19add265345944dd2056f5
gladyskankwala <gladyskankwala03@gmail.com>
2025-08-17 16:18:26 +0200

    add project files

---
+++ src/components/AnimatedTitlt.jsx
@@ -0,0 +1,12 @@
+
+
+function AnimatedTitlf() {
+    return(
+        <div className="mt-5 text-center tex-4xl uppercase
+        leading-[0.8] md:text-[6rem]">
+            Disc<b>o</b>ver the world's <br></br> l<b>a</b>rgest shared adventure
+        </div>
+    )
+}
+
+export default AnimatedTitlf
